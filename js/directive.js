ba4a4adeb99cde7d29db1aa5ac04e147e5e9fda4
Nick Jennings <nick@silverbucket.net>
2013-03-31 09:15:58 +0200

    handle data loading errors

--- js/directive.js
+++ js/directive.js
@@ -2,11 +2,12 @@ dogtalk.directive("error", function ($rootScope) {
   return {
     restrict: "E",
     template: '<div class="alert alert-error" ng-show="isRemoteStorageError">'+
-      '<strong>First things first</strong>' +
-      '<p>You need to connect to your remoteStorage</p>' +
-      '</div>',
+              '<strong>First things first</strong>' +
+              '<p>You need to connect to your remoteStorage</p>' +
+              '</div>',
     link: function (scope) {
       $rootScope.$on("$routeChangeError", function (event, current, previous, rejection) {
+        console.log('directive routeChangeError');
         scope.isRemoteStorageError = true;
       });
     }
