98b19207591e355c40f20d313a2be5a7e430db68
Nick Jennings <nick@silverbucket.net>
2013-09-02 16:56:48 +0200

    consolidation

--- res/js/controllers/log.js
+++
@@ -1,9 +0,0 @@
-/******
- * log
- ******/
-var logCtrl = dogtalk.controller("logCtrl",  ['$scope', '$route', '$routeParams', '$location',
-function ($scope, $route, $routeParams, $location) {
-  $scope.model = {
-    message: "this is the log page fool!"
-  };
-}] );
